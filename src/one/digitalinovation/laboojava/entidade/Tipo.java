9a3dff95b26ac73902b6c206f08902e60751ea8b
Andre <andre1993andre@hotmail.com>
2023-12-11 19:30:53 -0300

    colocando fatores no enum Tipo

--- src/one/digitalinovation/laboojava/entidade/Tipo.java
+++ src/one/digitalinovation/laboojava/entidade/Tipo.java
@@ -1,7 +1,18 @@
 package one.digitalinovation.laboojava.entidade;
 
 public enum Tipo {
-    M2,
-    M5,
-    M10;
+    M2 (2),
+    M5 (5),
+    M10 (10);
+
+    private double fator;
+
+    Tipo(double fator) {
+        this.fator = fator / 100;
+    }
+
+    public double getFator() {
+        return fator;
+    }
+
 }
